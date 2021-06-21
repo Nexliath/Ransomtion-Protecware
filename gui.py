ab54c765cc056723576f8eb61d89b4d021cf55ba
Sccor <stan.meline@gmail.com>
2021-06-21 14:49:31 +0200

    t

--- gui.py
+++ gui.py
@@ -1,32 +1,41 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QMessageBox, QDesktopWidget
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDesktopWidget, QMessageBox, QListWidget, QGridLayout
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 class Application(QWidget):
     def __init__(self):
-        super(Application, self).__init__()
+        super(Application,self).__init__()
         self.initUI()
 
     def initUI(self):
-        # init window
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Ransomtion Protecware')
+        self.setWindowTitle("Ransomtion Protecware")
         self.resize(1000, 600)
-
-        # centered window
+        layout = QGridLayout()
+        #centered window
         qtRectangle = self.frameGeometry()
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
-        self.show()
 
-        # Buttons
+        #buttons
         self.closeButton = QPushButton(self)
-        self.closeButton.setText("Close")  # text
-        self.closeButton.setShortcut('Ctrl+D')  # shortcut key
+        self.closeButton.setText("Close") #text
+        self.closeButton.setShortcut('Ctrl+D')  #shortcut key
         self.closeButton.clicked.connect(self.close)
-        self.closeButton.setToolTip("Close the widget")  # Tool tip
-        self.closeButton.move(100, 100)
+        self.closeButton.setToolTip("Close the widget") #Tool tip
+        self.closeButton.move(920,570) 
+
+        #list
+        self.listWidget = QListWidget()
+        self.listWidget.resize(300, 120)
+        self.listWidget.addItem("Item 1")
+        self.listWidget.addItem("Item 2")
+        self.listWidget.addItem("Item 3")
+        self.listWidget.addItem("Item 4")
+        self.listWidget.setWindowTitle('QListwidget Example')
+        self.listWidget.itemClicked.connect(self.clickedList)
+        layout.addWidget(self.listWidget)
 
     def closeEvent(self, event):
         reply = QMessageBox.question(self, 'Quit', 'Are you sure you want to quit?',
@@ -36,8 +45,14 @@ class Application(QWidget):
         else:
             event.ignore()
 
+    def clickedList(self, item):
+        QMessageBox.information(self, "ListWidget", "ListWidget: " + item.text())
+    
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Application()
+    ex.show()
     sys.exit(app.exec_())
