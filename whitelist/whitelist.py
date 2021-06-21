0435f6a9174ca8451466f6fced4b2f9c9fa7012c
Nexliath <victor.garnier@efrei.net>
2021-06-21 09:52:40 +0200

    Ajout whitelist

---
+++ whitelist/whitelist.py
@@ -0,0 +1,76 @@
+import sqlite3
+from sqlite3 import Error as Err
+
+
+def print_table_whitelist(conn):
+    c = conn.cursor()
+    req = c.execute('SELECT * FROM whitelist')
+
+    # print(req.fetchall())
+    print("Whitelist table : ")
+    for row in req.fetchall():
+        print(row)
+    print("\n")
+
+
+def verifPath(path, conn):
+    cur = conn.cursor()
+    cur.execute("SELECT w.path FROM whitelist w where path = ?;", [path])
+
+    if cur.fetchone():
+        return True
+    else:
+        return False
+
+
+def addToWhitelist(name, path, conn):
+    cur = conn.cursor()
+    if (verifPath(path, conn)):
+        print("Error add, alreay here")
+
+    else:
+        cur.execute("INSERT INTO whitelist(path,name) VALUES(?,?);",
+                    ((path), (name)))
+        conn.commit()
+
+
+def deleteFromWhitelist():
+    cur = conn.cursor()
+    if (verifPath(path, conn)):
+        cur.execute("INSERT INTO whitelist(path,name) VALUES(?,?);",
+                    ((path), (name)))
+        conn.commit()
+
+    else:
+        print("Error, not in the db")
+
+
+# TEST
+try:
+    # connect to the database
+    sqliteConnection = sqlite3.connect('Ransomtion-Protecware.db')
+    print("Database connection is established successfully!")
+
+    # connect a database connection to the
+    # database that resides in the memory
+    dest = sqlite3.connect(':memory:')
+    sqliteConnection.backup(dest)
+    print("Established database connection to a database that resides in the memory!")
+
+    print_table_whitelist(sqliteConnection)
+    print(verifPath("/usr/local/bin/test8", sqliteConnection))
+
+    addToWhitelist("test10", "/etc/bin/test10", sqliteConnection)
+
+    print_table_whitelist(sqliteConnection)  # db print
+
+except Err:
+    print(Err)
+
+finally:
+    sqliteConnection.close()
+
+
+# verify if a path is already in the db
+
+# add the name and the path of the desired process
