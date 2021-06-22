22db43a4f32dd0df63af3f546935d763c976d245
Nexliath <victor.garnier@efrei.net>
2021-06-22 14:15:02 +0200

    Whitelist integration GUI done

--- testTK.py
+++ testTK.py
@@ -2,13 +2,14 @@ from tkinter import *
 import sqlite3
 from sqlite3 import Error as Err
 
+
 class Logiciel(object):
     path = ""
     name = ""
+
     def __init__(self, newPath, newName):
         self.path = newPath
         self.name = newName
-
 
 
 def changeColorsNB():
@@ -20,6 +21,7 @@ def changeColorsNB():
     label_subtitle["fg"] = 'darkgrey'
     window.config(background="white")
 
+
 def changeColors():
     button["fg"] = '#1B2B4B'
     button["bg"] = '#E07B6A'
@@ -29,7 +31,7 @@ def changeColors():
     label_subtitle["fg"] = '#E07B6A'
     window.config(background='#1B2B4B')
 
-#initialisation de la whitelist
+# initialisation de la whitelist
 def initWhiteList():
 
     try:
@@ -40,11 +42,9 @@ def initWhiteList():
         req = c.execute('SELECT name FROM whitelist')
         i = 0
         for row in req.fetchall():
-            print(row)
             white_list.insert(i, row[0])
-            i=i+1
-            print(i)
-            
+            i = i + 1
+
     except sqlite3.Error as er:
         print('SQLite error: %s' % (' '.join(er.args)))
         print("Exception class is: ", er.__class__)
@@ -54,14 +54,15 @@ def initWhiteList():
     finally:
         sqliteConnection.close()
 
+
 # window
 window = Tk()
 window.title("Ransomtion Proteware")
-#window.geometry("1080x720")
-#window.minsize(1080,720)
+# window.geometry("1080x720")
+# window.minsize(1080,720)
 window.config(background='#1B2B4B')
 
-#menu
+# menu
 menu_bar = Menu(window)
 file_menu = Menu(menu_bar, tearoff=0)
 file_menu.add_command(label="Quitter", command=window.quit)
@@ -73,13 +74,16 @@ menu_bar.add_cascade(label="Propriétés", menu=prop_menu)
 window.config(menu=menu_bar)
 
 # Titre
-label_title = Label(window, text="Ransomtion Protecware", font=("Space Ranger", 40), bg='#1B2B4B', fg='#E07B6A', pady=20)
+label_title = Label(window, text="Ransomtion Protecware", font=(
+    "Space Ranger", 40), bg='#1B2B4B', fg='#E07B6A', pady=20)
 
 # Sous titre
-label_subtitle = Label(window, text="Logiciel actif...", font=("Space Ranger", 25), bg='#1B2B4B', fg='#E07B6A', pady=15)
+label_subtitle = Label(window, text="Logiciel actif...", font=(
+    "Space Ranger", 25), bg='#1B2B4B', fg='#E07B6A', pady=15)
 
 # Bouton éteindre
-button = Button(window, text="eteindre", font=("Space Ranger", 18), bg='#E07B6A', fg='#1B2B4B', command=window.destroy)
+button = Button(window, text="eteindre", font=("Space Ranger", 18),
+                bg='#E07B6A', fg='#1B2B4B', command=window.destroy)
 
 # whitelist
 white_list = Listbox(window, bg='#E07B6A', fg='#1B2B4B', bd=0)
