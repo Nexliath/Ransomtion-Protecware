c4a86219e26977cf76918ad3aed5e36cffc2ca68
Sccor <stan.meline@gmail.com>
2021-06-24 16:28:36 +0200

    enlevage de la commande ls et utilisation des fonctions python

--- interfaces.py
+++ interfaces.py
@@ -1,6 +1,6 @@
 import os
-list = os.popen("ls /sys/class/net").read()
-words = list.split()
+words = os.listdir("/sys/class/net")
 for interface in words :
 	cmd = "ip link set " + interface+ " down"
 	os.system(cmd)
+
