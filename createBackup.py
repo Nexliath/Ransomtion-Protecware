2af35c697e14fbf769e685a7e65532134cfd4a32
Irraky <drecours@student.42.fr>
2021-06-24 14:38:27 +0200

    correct init and get info for backup in createBackup.py

--- createBackup.py
+++ createBackup.py
@@ -1,9 +1,8 @@
-from os import X_OK
 import os.path
 import getpass
 
 homedir = os.path.expanduser("~")
-backupFile = backupFile = os.path.exists(homedir+"/.backup.sh")
+backupFile = os.path.exists(homedir + "/.backup.sh")
 
 def initBackup():
     username = getpass.getuser()
@@ -14,10 +13,21 @@ def initBackup():
     if check == 'n':
         print(f"Enter the IP address of the nas (let blank if none): ")
         ip_address = input()
+    if ip_address == "":
+        return
+    print(f"Enter the folder for the backup (let blank for /): ")
+    folder = input()
+    print(f"Are you sure the folder " + folder + " is correct ? (Y-n)")
+    check = input()
+    if check == 'n':
+        print(f"Enter the folder for the backup (let blank for /): ")
+        folder = input()
+    if folder == "":
+        folder = "/"
     f = open(homedir + "/.backup.sh", "x")
     f.write("#!/usr/bin/env bash")
     f.write("\nIP=" + ip_address)
-    f.write("\nFOLDER=/")
+    f.write("\nFOLDER=" + folder)
     f.write("\nssh \"nas@$IP\" \"/home/nas/backups.sh newMonth\"")
     f.write("\nrsync -avzpH --partial --delete \"$FOLDER\" \"nas@$IP:/home/nas/current/\"")        
     f.write("\nssh \"nas@$IP\" \"/home/nas/backups.sh hardLink\"\n")
@@ -27,20 +37,28 @@ def initBackup():
     line = "0 4 * * 1   " + homedir + "/.backup.sh"
     os.system("(crontab -u " + username + " -l; echo \"" + line + "\" ) | crontab -u " + username + " -") 
     os.system("sudo service cron reload")
- 
+
 def restoreBackup():
     print(f"Creating Backup file")
     f = open(homedir + "/.backup.sh")
-    data = f.readlines() [2]
+    f.readline()
+    ip = f.readline()
+    folder = f.readline() 
     f.close()
-    print (data)
+    ip = ip[3:]
+    folder = folder[7:]
+    print(f"fetching from ip: " + ip)
+    print(f"folder: " + folder)
 
 
 def main():
+    global backupFile
     if backupFile == False:
         initBackup()
+    backupFile = os.path.exists(homedir + "/.backup.sh")
+    if backupFile == True:
         restoreBackup()
-    
+
 
 if __name__ == "__main__":
     main()
