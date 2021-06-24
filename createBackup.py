58796e2cf3592fe2cdf46e785b20df23097b231b
irraky <drecours@student.42.fr>
2021-06-24 13:51:32 +0200

    WIP initialisation

--- createBackup.py
+++ createBackup.py
@@ -1,11 +1,11 @@
 import os.path
 import getpass
 
-username = getpass.getuser()
 homedir = os.path.expanduser("~")
-backupFile = os.path.exists(homedir+"/.backup.sh")
+backupFile = backupFile = os.path.exists(homedir+"/.backup.sh")
 
-if backupFile == False:
+def initBackup():
+    username = getpass.getuser()
     print(f"Enter the IP address of the nas (let blank if none): ")
     ip_address = input()
     print(f"Are you sure the address " + ip_address + " is correct ? (Y-n)")
@@ -18,7 +18,7 @@ if backupFile == False:
     f.write("\nIP=" + ip_address)
     f.write("\nFOLDER=/")
     f.write("\nssh \"nas@$IP\" \"/home/nas/backups.sh newMonth\"")
-    f.write("\nrsync -avzpH --partial --delete \"$FOLDER\" \"nas@$IP:/home/nas/current/\"")
+    f.write("\nrsync -avzpH --partial --delete \"$FOLDER\" \"nas@$IP:/home/nas/current/\"")        
     f.write("\nssh \"nas@$IP\" \"/home/nas/backups.sh hardLink\"\n")
     f.close()
 
@@ -26,3 +26,15 @@ if backupFile == False:
     line = "0 4 * * 1   " + homedir + "/.backup.sh"
     os.system("(crontab -u " + username + " -l; echo \"" + line + "\" ) | crontab -u " + username + " -") 
     os.system("sudo service cron reload")
+ 
+def initRestoreBackup():
+    print(f"Creating Backup file")
+
+def main():
+    if backupFile == False:
+        initBackup()
+        initRestoreBackup()
+    
+
+if __name__ == "__main__":
+    main()
