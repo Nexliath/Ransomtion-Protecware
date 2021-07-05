0d2aba85ff612ebbf9f3da523d1899a5b26e1e76
Aurélien <au2001sfg@gmail.com>
2021-07-05 16:28:57 +0200

    Fix decryptors test main

--- daemon/decryptors/__init__.py
+++ daemon/decryptors/__init__.py
@@ -8,9 +8,3 @@ def decrypt(ram_dump_path):
 		pass
 
 	return False
-
-if __name__ == "__main__":
-	if len(sys.argv) >= 2:
-		decrypt(sys.argv[1])
-	else:
-		print("Usage: python3 decryptors <path to memory dump>")
