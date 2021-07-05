68adbc494fb44db62f20405f4579d70db64f48a0
Aurélien <au2001sfg@gmail.com>
2021-07-05 18:48:45 +0200

    Improve GonnaCry key finder

--- daemon/decryptors/gonnacry.py
+++ daemon/decryptors/gonnacry.py
@@ -4,41 +4,56 @@ import base64
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-bit_len = 2048
 ransomware_name = "gonnacry"
 home = os.path.expanduser("~")
 ransomware_path = os.path.join(home, ransomware_name)
 aes_encrypted_keys_path = os.path.join(ransomware_path, "AES_encrypted_keys.txt")
 client_public_key_path = os.path.join(ransomware_path, "client_public_key.PEM")
 
+bit_len = 2048
+pem_len = 380
+pem_prefix = "ssh-rsa "
+pem_prefix_len = len("ssh-rsa ")
+pem_prefix_bytes = bytes(pem_prefix, "ascii")
+
+def test_key(data):
+	try:
+		decrypted_test = "Hello, World!".encode("utf-8")
+
+		with open(client_public_key_path, "r") as f:
+			public_key = RSA.importKey(f.read())
+		cipher = PKCS1_OAEP.new(public_key)
+		encrypted_test = cipher.encrypt(decrypted_test)
+
+		private_key = RSA.importKey(data)
+		cipher = PKCS1_OAEP.new(private_key)
+		return cipher.decrypt(encrypted_test) == decrypted_test
+	except:
+		return False
+
 def find_key(ram_dump_path):
-	decrypted_test = "Hello, World!".encode("utf-8")
-
-	with open(client_public_key_path, "r") as f:
-		public_key = RSA.importKey(f.read())
-	cipher = PKCS1_OAEP.new(public_key)
-	encrypted_test = cipher.encrypt(decrypted_test)
-
 	with open(ram_dump_path, "rb") as f:
 		ram_dump = f.read()
 
-	data = b"\0" + ram_dump[:bit_len // 8 - 1]
-	for i in range(bit_len // 8, len(ram_dump)):
-		data = data[1:] + ram_dump[i]
+	try:
+		i = 0
+		while True:
+			i = ram_dump.index(pem_prefix_bytes, i)
+			try:
+				found_key = ram_dump[i:i + pem_len].decode("ascii")
+				if test_key(found_key):
+					return found_key
+			except:
+				pass
 
-		private_key = RSA.importKey(data)
-		cipher = PKCS1_OAEP.new(public_key)
-		if cipher.decrypt(decrypted_test) == decrypted_test:
-			return private_key
-
-		print(i / len(ram_dump) * 100)
+			i += pem_prefix_len
+	except:
+		pass
 
 def decrypt(ram_dump_path):
 	private_key = find_key(ram_dump_path)
 	if private_key is None:
 		return False
-
-	print(private_key)
 
 	keys = []
 	with open(aes_encrypted_keys_path, "r") as f:
@@ -47,8 +62,12 @@ def decrypt(ram_dump_path):
 			path = base64.b64decode(base64_path).decode("utf-8")
 			keys.append((encrypted_aes_key, path))
 
+	rsa_key = RSA.importKey(private_key)
+	cipher = PKCS1_OAEP.new(rsa_key)
 	for encrypted_aes_key, path in keys:
-		pass # TODO
+		aes_key = cipher.decrypt(encrypted_aes_key)
+
+		print(aes_key, path) # TODO: Decrypt file
 
 	return True
 
